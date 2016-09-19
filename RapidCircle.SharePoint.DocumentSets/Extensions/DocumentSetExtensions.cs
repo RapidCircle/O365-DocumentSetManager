43d7086321c3622f84cb772639f9772e41565e27
Daniel McPherson <daniel.mcpherson@rapidcircle.com>
2016-09-19 19:49:58 +0200

    Large Upload Support

--- RapidCircle.SharePoint.DocumentSets/Extensions/DocumentSetExtensions.cs
+++ RapidCircle.SharePoint.DocumentSets/Extensions/DocumentSetExtensions.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.SharePoint.Client;
 using Microsoft.SharePoint.Client.DocumentSet;
+using File = Microsoft.SharePoint.Client.File;
 
 namespace RapidCircle.SharePoint.DocumentSets.Extensions
 {
@@ -74,47 +76,148 @@ namespace RapidCircle.SharePoint.DocumentSets.Extensions
 
                 MemoryStream memStream = new MemoryStream();
                 documentStream.Value.CopyTo(memStream);
-                memStream.Seek(0, SeekOrigin.Begin);
-                Microsoft.SharePoint.Client.File.SaveBinaryDirect((ClientContext)ctx, webUrl.AbsolutePath + "/_cts/" + documentSetName + "/" + documentItem.File.Name, memStream, true);
-                memStream.Close();
+                memStream.Position = 0;
+                //File.SaveBinaryDirect((ClientContext)ctx, webUrl.AbsolutePath + "/_cts/" + documentSetName + "/" + documentItem.File.Name, memStream, true);
+                //memStream.Close();
+
+                UploadFile((ClientContext)ctx, webUrl.AbsolutePath + "/_cts/" + documentSetName, memStream, documentItem.File.Name);
+                //memStream.Close();
             }
         }
 
-        //public static void AddMajorVersion(this DocumentSetTemplate documentSet, ListItem documentItem, string documentSetName, string folder, ContentTypeId docId)
-        //{
-        //    var ctx = (ClientContext)documentSet.Context;
+        private static void UploadFile(ClientContext ctx, string folderPath, MemoryStream memStream, string fileName)
+        {
+            Guid uploadId = Guid.NewGuid();
+            File uploadFile;
+            //int blockSize = 1000;
+            int blockSize = 1 * 1024 * 1024;
 
-        //    ctx.Load(documentItem.File.Versions);
-        //    ctx.ExecuteQuery();
+            Folder uploadFolder = ctx.Web.GetFolderByServerRelativeUrl(folderPath);
+            ctx.Load(uploadFolder, f => f.ServerRelativeUrl);
+            ctx.ExecuteQuery();
 
-        //    string majorVersion = documentItem.File.MajorVersion.ToString() + ".0";
+            long fileSize = memStream.Length;
 
-        //    ClientResult<Stream> documentStream = null;
-        //    foreach (FileVersion fileVersion in documentItem.File.Versions)
-        //    {
-        //        if (fileVersion.VersionLabel.Equals(majorVersion))
-        //        {
-        //            documentStream = fileVersion.OpenBinaryStream();
-        //            ctx.ExecuteQuery();
-        //            break;
-        //        }
-        //    }
+            if (fileSize <= blockSize)
+            {
+                // Use regular approach
+                //using (FileStream fs = new FileStream(fileName, FileMode.Open))
+                //{
+                    FileCreationInformation fileInfo = new FileCreationInformation();
+                    fileInfo.ContentStream = memStream;
+                    fileInfo.Url = fileName;
+                    fileInfo.Overwrite = true;
+                    uploadFile = uploadFolder.Files.Add(fileInfo);
+                    ctx.Load(uploadFile);
+                    ctx.ExecuteQuery();
+                    // return the file object for the uploaded file
+                    //return uploadFile;
+                //}
+            }
+            else
+            {
 
-        //    string placeholderPage = "Placeholder";
-        //    MemoryStream repo = new MemoryStream(Encoding.UTF8.GetBytes(placeholderPage));
-        //    documentSet.DefaultDocuments.Add(folder + documentItem.File.Name, docId, repo.ToArray());
-        //    documentSet.Update(true);
-        //    ctx.ExecuteQuery();
-        //    repo.Close();
+                ClientResult<long> bytesUploaded = null;
+                FileStream fs = null;
+                try
+                {
+                    //fs = System.IO.File.Open(fileName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                    using (BinaryReader br = new BinaryReader(memStream))
+                    {
+                        byte[] buffer = new byte[blockSize];
+                        Byte[] lastBuffer = null;
+                        long fileoffset = 0;
+                        long totalBytesRead = 0;
+                        int bytesRead;
+                        bool first = true;
+                        bool last = false;
 
-        //    MemoryStream memStream = new MemoryStream();
-        //    documentStream.Value.CopyTo(memStream);
-        //    memStream.Seek(0, SeekOrigin.Begin);
+                        // Read data from filesystem in blocks 
+                        while ((bytesRead = br.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            totalBytesRead = totalBytesRead + bytesRead;
 
-        //    Uri url = new Uri(ctx.Url);
+                            // We've reached the end of the file
+                            if (totalBytesRead == fileSize)
+                            {
+                                last = true;
+                                // Copy to a new buffer that has the correct size
+                                lastBuffer = new byte[bytesRead];
+                                Array.Copy(buffer, 0, lastBuffer, 0, bytesRead);
+                            }
 
-        //    Microsoft.SharePoint.Client.File.SaveBinaryDirect((ClientContext)ctx, url.AbsolutePath + "/_cts/" + documentSetName + "/" + documentItem.File.Name, memStream, true);
-        //    memStream.Close();
-        //}
+                            if (first)
+                            {
+                                using (MemoryStream contentStream = new MemoryStream())
+                                {
+                                    // Add an empty file.
+                                    FileCreationInformation fileInfo = new FileCreationInformation();
+                                    fileInfo.ContentStream = contentStream;
+                                    fileInfo.Url = fileName;
+                                    fileInfo.Overwrite = true;
+                                    uploadFile = uploadFolder.Files.Add(fileInfo);
+
+                                    // Start upload by uploading the first slice. 
+                                    using (MemoryStream s = new MemoryStream(buffer))
+                                    {
+                                        // Call the start upload method on the first slice
+                                        bytesUploaded = uploadFile.StartUpload(uploadId, s);
+                                        ctx.ExecuteQuery();
+                                        // fileoffset is the pointer where the next slice will be added
+                                        fileoffset = bytesUploaded.Value;
+                                    }
+
+                                    // we can only start the upload once
+                                    first = false;
+                                }
+                            }
+                            else
+                            {
+                                // Get a reference to our file
+                                uploadFile =
+                                    ctx.Web.GetFileByServerRelativeUrl(uploadFolder.ServerRelativeUrl +
+                                                                       System.IO.Path.AltDirectorySeparatorChar +
+                                                                       fileName);
+
+                                if (last)
+                                {
+                                    // Is this the last slice of data?
+                                    using (MemoryStream s = new MemoryStream(lastBuffer))
+                                    {
+                                        // End sliced upload by calling FinishUpload
+                                        uploadFile = uploadFile.FinishUpload(uploadId, fileoffset, s);
+                                        ctx.ExecuteQuery();
+
+                                        // return the file object for the uploaded file
+                                        //return uploadFile;
+                                    }
+                                }
+                                else
+                                {
+                                    using (MemoryStream s = new MemoryStream(buffer))
+                                    {
+                                        // Continue sliced upload
+                                        bytesUploaded = uploadFile.ContinueUpload(uploadId, fileoffset, s);
+                                        ctx.ExecuteQuery();
+                                        // update fileoffset for the next slice
+                                        fileoffset = bytesUploaded.Value;
+                                    }
+                                }
+                            }
+
+                        } // while ((bytesRead = br.Read(buffer, 0, buffer.Length)) > 0)
+                    }
+                }
+                finally
+                {
+                    if (fs != null)
+                    {
+                        fs.Dispose();
+                    }
+                }
+
+            }
+
+        }
     }
 }
